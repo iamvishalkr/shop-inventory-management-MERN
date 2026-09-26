b40b2db18bf4a94cf793f31ee8d4b6f6b5213f2c
iamvishalkr <roysajal@yandex.com>
2026-09-26 13:32:52 +0530

    check env keys

--- server/src/lib/email.ts
+++ server/src/lib/email.ts
@@ -13,5 +13,10 @@ export const transporter = nodemailer.createTransport({
 } as TransportOptions);
 
 
-
-export const resend = new Resend(process.env.RESEND_API_KEY);
+const api_key = process.env.RESEND_API_KEY;
+if (api_key) {
+    console.log("GOT API KEY");
+}else{
+    console.log("API KEY NOT FOUND");
+}
+export const resend = new Resend(api_key);
